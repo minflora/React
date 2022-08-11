8bb012e7f203b27ebb9e6d51b8e2b470a8369cad
minflora <wleka0402@gmail.com>
2022-08-11 20:06:54 +0900

    habit

---
+++ src/components/habit.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Habit extends Component {
+    render() {
+        return(
+        <>
+        <span className="habbit-name">Reading</span>
+        <span className="habbit-count">8</span>
+        </>
+        );
+    }
+}
+
+export default Habit;
